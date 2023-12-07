8b35f8a7bc742075894641c1b51937b444c4eed3
Giriraj25 <me@example.com>
2023-12-07 18:20:49 +0400

    Initial commit

---
+++ week 3/exc2.js
@@ -0,0 +1,9 @@
+
+//Define the sentence array
+const sentence = ["my", "favorite", "color", "is", "blue"];
+
+//Join array elements into a single sentence
+const joinedSentence = sentence.join(' ');
+
+//Output the joined sentence
+console.log(joinedSentence); // Output: "my favorite color is blue"
